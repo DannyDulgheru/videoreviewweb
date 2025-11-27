6e4295c4ad1d7c6e9fcf84a845802bf15408767c
DannyDulgheru <contact.dulgheru.dan@gmail.com>
2025-11-27 08:28:21 +0000

    update

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,12 +1,9 @@
 import VideoListPage from '@/components/video-list-page';
-import AuthGuard from '@/components/auth-guard';
 
 export default function List() {
   return (
-    <AuthGuard>
-        <main className="flex min-h-screen flex-col items-center p-4 sm:p-8 md:p-12 bg-background">
-            <VideoListPage />
-        </main>
-    </AuthGuard>
+    <main className="flex min-h-screen flex-col items-center p-4 sm:p-8 md:p-12 bg-background">
+      <VideoListPage />
+    </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import UploadPage from '@/components/upload-page';
-import AuthGuard from '@/components/auth-guard';
 
 export default function Home() {
   return (
-    <AuthGuard>
-        <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-12 bg-background">
-            <UploadPage />
-        </main>
-    </AuthGuard>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-12 bg-background">
+      <UploadPage />
+    </main>
   );
 }
